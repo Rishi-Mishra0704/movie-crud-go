7984dac5f14309d339aea5c7edbdc1f03adbbce6
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-27 00:02:43 +0530

    some change 2

--- main.go
+++ main.go
@@ -32,6 +32,6 @@ func main() {
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
-	fmt.Println("Starting server on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	fmt.Println("Starting server on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
