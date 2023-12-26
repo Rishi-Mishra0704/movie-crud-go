a2d373b62e0f3f268521762524f40dc8a4bb6f35
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-26 23:53:32 +0530

    add delete and fetch single movie

--- delete_movie.go
+++ delete_movie.go
@@ -1,8 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Delete a movie
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, movie := range movies {
+		if movie.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
 }
--- get_movie.go
+++ get_movie.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Get all movies
@@ -12,5 +14,14 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 }
 
 func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	// Loop through movies and find one with the id from the params
+	for _, movie := range movies {
+		if movie.ID == params["id"] {
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
 
 }
