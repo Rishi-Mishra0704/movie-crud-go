7a93d1bc120222996f5603d9ed136551eeb0ea29
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-26 23:59:20 +0530

    add logic to create movie

--- create_movie.go
+++ create_movie.go
@@ -1,9 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
 
 // Create a new movie
 func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(1000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+
 }
 func updateMovie(w http.ResponseWriter, r *http.Request) {
 }
