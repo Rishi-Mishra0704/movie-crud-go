464335b0f7ce59a660d6ee9d28d87aa279d7a24c
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-26 23:49:38 +0530

    add fetch all movies logic

--- get_movie.go
+++ get_movie.go
@@ -1,10 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 // Get all movies
 func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
 }
 
 func getMovie(w http.ResponseWriter, r *http.Request) {
+
 }
